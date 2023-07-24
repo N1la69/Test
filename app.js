86c4cb46153f1e853b75693191d1d609c56c993e
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2023-07-24 20:05:07 +0530

    new

---
+++ app.js
@@ -0,0 +1,28 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+
+const section = document.querySelector('section');
+const end = section.querySelector('h1');
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+    duration: 14000,
+    triggerElement: intro,
+    triggerHook: 0
+}).setPin(intro).addTo(controller);
+
+let accel = .2;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', e => {
+    scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(()=>{
+    delay += (scrollpos - delay) * accel;
+
+    video.currentTime = scrollpos;
+},33.33)
